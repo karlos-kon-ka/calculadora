f7a4ddd837109b0a25794faeb5f2161edb814e5c
Karlos <143749395+karlos-kon-ka@users.noreply.github.com>
2023-09-18 04:07:43 +0200

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const pantalla = document.querySelector(".pantalla");
 const botones = document.querySelectorAll(".btn");
 
@@ -40,4 +41,48 @@ botones.forEach(boton => {
         
 
     })
+=======
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".btn");
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const botonApretado = boton.textContent;
+
+        if(boton.id === "C"){
+            pantalla.textContent ="0";
+            return;
+        }
+        if(boton.id === "borrar") {
+            if(pantalla.textContent.length === 1 || pantalla.textContent === "Error"){
+                pantalla.textContent = "0"; 
+            } else {
+
+    
+            pantalla.textContent = pantalla.textContent.slice(0, -1);
+            return;
+        }
+        }
+
+        if(boton.id === "igual") { 
+            try{
+            pantalla.textContent = eval(pantalla.textContent);
+        } catch {
+            pantalla.textContent = "Error";
+           
+        } return;
+        }
+
+       if(pantalla.textContent === "0") {
+        pantalla.textContent = botonApretado;
+       } else{
+        pantalla.textContent += botonApretado;
+       }
+
+
+        
+        
+
+    })
+>>>>>>> 9a28724a862f6d6336de2cbd6f93aaca58b422c8
 } )
