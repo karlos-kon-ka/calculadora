3b05acaa0e3c64c93b3f73604c3f16f31a40ce71
Karlos <143749395+karlos-kon-ka@users.noreply.github.com>
2023-10-28 21:42:12 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,7 @@
+contrasera_almacenada = 'elreymenda'
+contraseña_escrita = 'elreymenda'
+
+
+if contrasera_almacenada == contraseña_escrita:
+    print('todo bien papi')
+else: print('No esta todo bien papi')    
